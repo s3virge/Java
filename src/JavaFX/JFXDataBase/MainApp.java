01e7dd98bca2d4cb1062532aab27f2addc5570b6
S3ViRGE <kobzarvit@gmail.com>
2017-08-11 14:00:18 +0300

    I'm working with Java

--- src/JavaFX/JFXDataBase/MainApp.java
+++ src/JavaFX/JFXDataBase/MainApp.java
@@ -45,6 +45,7 @@ public class MainApp extends Application {
      * Returns the data as an observable list of Persons.
      * @return
      */
+    //наблюдаемый список
     public ObservableList<Person> getPersonData() {
         return personData;
     }
